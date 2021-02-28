795e88c0ac602e19a27755bf3820bb369b954f16
beebertaolive <68178210+berta787@users.noreply.github.com>
2021-02-27 23:33:05 -0700

    tried to make flipping cards with JS - didn't work :(

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 $('.collapse').collapse()
 
+// const cardsUpgradezz = document.querySelectorAll(".cards__single");
+
+// function flipCard() {
+//   this.classList.toggle("flip");
+// }
+// cardsUpgradezz.forEach((card) => card.addEventListener("click", flipCard));
+
